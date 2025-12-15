0780ab5653352d8f5d5925f47f83d0ea104fef89
Logan-Vargas <156100152+Logan-Vargas@users.noreply.github.com>
2025-12-15 20:54:12 +0000

    added more

---
+++ day_1_activities/student_data2.py
@@ -0,0 +1,11 @@
+students = [
+    {
+    "CPSID": 10000011,
+    "Combo,Name": "Anderson, Karen",
+    "LName": "Anderson",
+    "FName": "Karen",
+    "MName": "Lynn",
+    "HR": "B211",
+    "GL": 10,
+    "Email": ["karenanderson@example.com", "karen.a@example.net"]
+}
